b353e3375f0079de24f305494607868776ce30bd
rezikalf2 <rezikalf2@gmail.com>
2018-11-23 19:55:42 +0300

    added case studies on home page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,6 +22,20 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+<section class="featured-work">
+	<div class="case-studies-on-home">
+		<h3>Featured Work</h3>
+		<?php query_posts('post_type=case_studies&posts_per_page=3') ?>
+		<?php while ( have_posts() ) : the_post(); 
+			$size = "full";
+			$image_1 = get_field('image-1'); ?>
+			<div class="each-case-on-home">
+				<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image( $image_1, $size ); ?> </a>
+				<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+			</div>
+       <?php endwhile; ?> 
+	</div>
+</section>
 <section class="recent-posts">
  <div class="site-content">
   <div class="blog-post">
@@ -37,3 +51,23 @@ get_header(); ?>
 </section>
 
 <?php get_footer(); ?>
+<!--<section class="featured-work">
+	<div class="site-content">
+	<div class="blog-post">
+		<h4>Featured Work</h4>
+		<?php //query_posts('post_type=case_studies&posts_per_page=3'); ?>
+	  	<?php //while ( have_posts() ) : the_post(); 
+			//$image_1 = get_field('image_1');
+			//$size = "full";
+			?>
+			<figure>
+			<?php //if($image_1) { ?>
+				<?php //echo wp_get_attachment_image( $image_1, 'medium' ); ?>
+			<?php// } ?>
+			</figure>
+			<h3><a href="<?php// the_premalink() ?>"><?php //the_title(); ?></a><h3>
+     	<?php// endwhile; ?> 
+    	<?php //wp_reset_query(); ?>
+	</div>
+	</div>
+</section>-->
---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content sidebar">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post(); 
+		$size = "full";
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image_1 = get_field('image-1');
+		$image_2 = get_field('image-2');
+		$image_3 = get_field('image-3'); ?>
+			<article class="case-study">
+				<aside class="case-study-sidebar">
+
+					<h2><?php the_title(); ?></h2>
+					<h4><?php echo $services; ?></h4>
+					<h4><?php echo $client;?></h4>
+					<?php the_content(); ?>
+					<p><a href="<?php echo $link;?>">Site Link</a></p>
+				</aside>
+				<div class="case-study-images">
+					<?php if($image_1) { ?>
+						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+					<?php } ?>
+					<?php if($image_2) { 
+						echo wp_get_attachment_image( $image_2, $size );
+					 } ?>
+					<?php if($image_3) { 
+						echo wp_get_attachment_image( $image_3, $size );
+					 } ?>
+				</div>
+			</article>
+
+
+
+
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+
+		
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
